9801732e65991c19127e21fb36fccb87b25bb486
adamhochberger <adamhochberger@gmail.com>
2022-11-24 13:44:34 -0500

    Refactor base methods into separate class and breakup Smite/Paladins functions

--- smite_api_wrapper/SmiteApiMethods.py
+++ smite_api_wrapper/SmiteApiMethods.py
@@ -1,10 +1,24 @@
-class SmiteApiMethods:
+class HirezApiMethods:
+    # Connectivity, Development, And System Status
     PING = "/ping{ResponseFormat}"
     CREATE_SESSION = "/createsession{ResponseFormat}/{developerId}/{signature}/{timestamp}"
     TEST_SESSION = "/testsession[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}"
     GET_DATA_USED = "/getdataused[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}"
     GET_HIREZ_SERVER_STATUS = "/gethirezserverstatus[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}"
     GET_PATCH_INFO = "/getpatchinfo[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}"
+
+
+class SmiteApiMethods(HirezApiMethods):
+    # Gods, And Items
+    GET_GODS = "/getgods[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{languageCode}"
+    GET_GOD_LEADERBOARD = "/getgodleaderboard[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{godId}/{queue}"  # only queues 440, 450, 451
+
+
+class PaladinsApiMethods(HirezApiMethods):
+    # Champions, And Items
+    GET_CHAMPIONS = "/getchampions[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{languageCode}"
+    GET_CHAMPION_CARDS = "/getchampions[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{languageCode}"
+    GET_CHAMPION_LEADERBOARD = "/getchampionleaderboard[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{championId}/{queue}"  # only queue 428
 
 
 def get_list_of_params(method_name: str):
